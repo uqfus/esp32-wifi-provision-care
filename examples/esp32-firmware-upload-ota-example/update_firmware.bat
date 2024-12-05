8005af7310e913c5e7e56aeed858fe4b6d206612
unknown <4chemist@gmail.com>
2024-12-05 17:10:52 +0300

    OTA sample

--- examples/esp32-firmware-upload-ota-example/update_firmware.bat
+++ examples/esp32-firmware-upload-ota-example/update_firmware.bat
@@ -1,1 +1,1 @@
-curl --data-binary @build\esp32-firmware-upload-ota-example.bin http:/esp32-firmware-upload-ota-example.local/updateota
+curl --data-binary @build\esp32-firmware-upload-ota-example.bin http://esp32-firmware-upload-ota-example.local/updateota
